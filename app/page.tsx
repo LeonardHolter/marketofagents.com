e162183c44767be023f35f5304e88df76307c42e
LeonardHolter <leonard.holter@columbia.edu>
2025-01-09 17:11:33 +0100

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,3 @@
 export default function Home() {
-  return (
-    <div className="navbar bg-base-100">
-      <div className="flex-1">
-        <a className="btn btn-ghost text-xl">MarketOfAgents</a>
-      </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Browse</a>
-          </li>
-          <li>
-            <details>
-              <summary>Settings</summary>
-              <ul className="bg-base-100 rounded-t-none p-2">
-                <li>
-                  <a>Payment</a>
-                </li>
-                <li>
-                  <a>Account</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
+  return <button>hei </button>;
 }
