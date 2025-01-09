b1a67a0eae69fca77805f01650833b2a4eb8ec10
LeonardHolter <leonard.holter@columbia.edu>
2025-01-09 17:37:19 +0100

    placeholders

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Footer from "./components/Footer";
+
 export default function Home() {
-  return <button>hei </button>;
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Hero></Hero>
+      <Footer></Footer>
+    </div>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,6 @@ export default {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["nord"],
+    themes: ["dark"],
   },
 } satisfies Config;
