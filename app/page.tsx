9b25fd00a4aca21c49ffd69203be3c7ee21f80c3
LeonardHolter <leonard.holter@columbia.edu>
2025-01-10 13:37:45 +0100

    hero_remove

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import Navbar from "./components/Navbar";
 import AgentCardList from "./components/AgentCardList";
-import Hero from "./components/Hero";
 
 export default function Home() {
   return (
     <div>
       <Navbar></Navbar>
-      <Hero></Hero>
       <AgentCardList></AgentCardList>
     </div>
   );
