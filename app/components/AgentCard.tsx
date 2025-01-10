46e74fee6a53ff9bd2f5cb5cd69dc6b6703050be
LeonardHolter <leonard.holter@columbia.edu>
2025-01-10 14:27:10 +0100

    routing

--- app/components/AgentCard.tsx
+++ app/components/AgentCard.tsx
@@ -1,4 +1,4 @@
-export default function AgentCard() {
+export default function AgentCard({ children }) {
   return (
     <div className="card bg-base-100 w-64 shadow-xl hover:shadow-2xl hover:scale-105 transition-transform duration-300 ease-in-out">
       <figure>
@@ -9,13 +9,13 @@ export default function AgentCard() {
       </figure>
       <div className="card-body">
         <h2 className="card-title">
-          Shoes!
+          {children}
           <div className="badge badge-secondary">NEW</div>
         </h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <p>Makes your life 100% easier</p>
         <div className="card-actions justify-end">
-          <div className="badge badge-outline">Fashion</div>
-          <div className="badge badge-outline">Products</div>
+          <div className="badge badge-outline">CRM</div>
+          <div className="badge badge-outline">Marketing</div>
         </div>
       </div>
     </div>
--- app/components/AgentCardList.tsx
+++ app/components/AgentCardList.tsx
@@ -1,11 +1,15 @@
 import AgentCard from "./AgentCard";
+import Link from "next/link";
 export default function AgentList() {
   return (
     <div className="flex flew-wrap sm:flex-row sm:flex-wrap flex-row justify-center items-center gap-12 mt-20">
-      <AgentCard></AgentCard>
-      <AgentCard></AgentCard>
-      <AgentCard></AgentCard>
-      <AgentCard></AgentCard>
+      <Link href="/agent1">
+        {" "}
+        <AgentCard>Agent</AgentCard>{" "}
+      </Link>
+      <AgentCard>Agent</AgentCard>
+      <AgentCard>Agent</AgentCard>
+      <AgentCard>Agent</AgentCard>
     </div>
   );
 }
