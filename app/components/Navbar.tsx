1a35664cb22ac963a43ec157e6550e627e080e8a
LeonardHolter <leonard.holter@columbia.edu>
2025-01-10 11:19:33 +0100

    theme update

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,7 @@
 "use static";
 export default function Navbar() {
   return (
-    <div className="navbar bg-base-100">
+    <div className="navba bg-base-100">
       <div className="flex-1">
         <a className="btn btn-ghost text-xl">MarketOfAgents</a>
       </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,6 @@ export default {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["dark"],
+    themes: ["bumblebee"],
   },
 } satisfies Config;
