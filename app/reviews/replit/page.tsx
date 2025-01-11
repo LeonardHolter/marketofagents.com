2f4c75f8cf221693035a6ac67a8c8a8c5886a065
LeonardHolter <leonard.holter@columbia.edu>
2025-01-11 10:43:42 -0500

    added MOA

--- app/reviews/replit/page.tsx
+++ app/reviews/replit/page.tsx
@@ -104,7 +104,7 @@ const Page = () => {
       {/* Footer */}
       <footer className="bg-gray-800 text-white py-6">
         <div className="container mx-auto px-4 text-center">
-          <p>&copy; 2025 My Blog. All rights reserved.</p>
+          <p>&copy; 2025 MOA. All rights reserved.</p>
         </div>
       </footer>
     </div>
