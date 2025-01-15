bfb2f8c233c7103a730bc904d2c4b00ef6a3b190
LeonardHolter <leonard.holter@columbia.edu>
2025-01-14 19:57:23 -0500

    vertical mobile

--- app/components/AgentCardList.tsx
+++ app/components/AgentCardList.tsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 
 export default function AgentList() {
   return (
-    <div className="flex flew-wrap sm:flex-row sm:flex-wrap flex-row justify-center items-center gap-12 mt-20">
+    <div className="flex flex-col sm:flex-wrap sm:flex-row justify-center items-center gap-12 mt-20">
       <Link href="/agent1">
-        <AgentCard imagepath="/meme.png">Meme Agent</AgentCard>{" "}
+        <AgentCard imagepath="/meme.png">Meme Agent</AgentCard>
       </Link>
       <Link href="/replit_review">
-        <AgentCard imagepath="/replit.png">Replit Agent</AgentCard>{" "}
+        <AgentCard imagepath="/replit.png">Replit Agent</AgentCard>
       </Link>
     </div>
   );
