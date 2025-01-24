9231c82527d72040ef3c2d609d79221a4691d05c
LeonardHolter <leonard.holter@columbia.edu>
2025-01-24 09:54:37 -0500

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import NavBar from "./components/Navbar";
-import AgentCategories from "./components/AgentCategories"; // Adjust to { AgentCategories } if needed
 import AgentCardList from "./components/AgentCardList"; // Adjust to { AgentCardList } if needed
 
 export default function Home() {
